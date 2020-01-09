b758531c06bc00dbd961708f2d0a8cb92e8d6c68
illiagitdev <illya.posv.b@gmail.com>
2020-01-10 00:57:52 +0200

    work with date

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import services.Operational;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main extends Application {
 
@@ -15,5 +23,29 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
+        //------------------------------------------
+        String responseDate = "2013-06-19T12:05:22.000Z";
+        String responseUrl = "https://yt3.ggpht.com/-CwNoyH82d6s/AAAAAAAAAAI/AAAAAAAAAAA/8dAabLfhwMI/s88-c-k-no-mo-rj-c0xffffff/photo.jpg";
+        System.out.println("Response from YouTube Api: " + responseDate);
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SZ");
+        String result = responseDate;
+        try {
+            Date date = format.parse(responseDate);
+            System.out.println("Parsed date = " + date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Stage newStage = new Stage();
+        Pane newPane = new Pane();
+        Scene newScene = new Scene(newPane);
+        newStage.setScene(newScene);
+
+        ImageView imageView = new ImageView(new Image(responseUrl));
+        newPane.getChildren().addAll(imageView);
+        newStage.show();
+        //------------------------------------------
     }
 }
