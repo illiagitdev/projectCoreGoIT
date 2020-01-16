1300abf54a5e373a54d40c86224a66572bde5004
illia_dev <illya.posv.b@gmail.com>
2020-01-16 11:49:16 +0200

    fixed UI and text on button

--- src/main/java/messageBoxes/AlertBox.java
+++ src/main/java/messageBoxes/AlertBox.java
@@ -1,9 +1,10 @@
 package messageBoxes;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.*;
 
 public class AlertBox {
@@ -12,21 +13,23 @@ public class AlertBox {
         Stage window = new Stage();
         window.setTitle(title);
         window.initModality(Modality.APPLICATION_MODAL);
-        window.setMinWidth(250);
+//        window.setWidth(250);
 
         Label labelTitle = new Label(message);
-        Button button = new Button("Yes");
-        HBox layout = new HBox(10);
+        Button button = new Button("Ok!");
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(8, 8, 8, 8));
+        layout.setPrefWidth(250);
         layout.getChildren().addAll(labelTitle, button);
         layout.setAlignment(Pos.CENTER);
-
-        Scene scene = new Scene(layout);
 
         button.setOnMouseClicked(e -> {
             System.out.println("Alert: " + message);
             window.close();
         });
 
+        Scene scene = new Scene(layout);
         window.setScene(scene);
         window.showAndWait();
     }
