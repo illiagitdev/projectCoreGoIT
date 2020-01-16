cada70b8634880e968359d2d4c294d47abd5dd63
illia_dev <illya.posv.b@gmail.com>
2020-01-16 11:50:52 +0200

    cleared unused objects

--- src/main/java/services/OperationalFX.java
+++ src/main/java/services/OperationalFX.java
@@ -2,11 +2,9 @@ package services;
 
 import controlers.SearchControlsFX;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import ui.ChannelView;
 import ui.ResultsOutputLayoutFX;
 import ui.SearchLayoutFX;
 
@@ -18,8 +16,7 @@ public class OperationalFX {
 
     public void run(Stage window) {
         setupStage(window);
-//todo: scenes to switch if channel is implemented
-        Scene mainScene, channelScene;
+        Scene mainScene;
 
         // version 0.0.2
         SearchLayoutFX searchLayout = new SearchLayoutFX();
@@ -43,22 +40,11 @@ public class OperationalFX {
                 searchLayout.getDaysPublished().getText(),
                 searchView.getResultsList()));
 
+        // closing okhttp client on program close
         window.setOnCloseRequest(event -> {
             SearchControlsFX.getClient().dispatcher().executorService().shutdown();
             System.out.println("client OkHTTP was closed");
         });
-
-        // channel layout - switching with general results view like on/off by visibility - no good
-//        ChannelView channelLayout = new ChannelView();
-        ResultsOutputLayoutFX channelView = new ResultsOutputLayoutFX(WIDTH, HEIGHT, 210);
-
-//        VBox layout2 = new VBox();
-        // here search window no more accessible if added to second layout
-//        layout2.getChildren().addAll(searchLayout.searchPanel(), channelLayout.newChannelPane(), channelView.getResultsBox());
-
-//        Pane pane2 = new Pane();
-//        pane2.getChildren().add(layout2);
-//        channelScene = new Scene(pane2);
     }
 
     private void setupStage(Stage stage) {
